32b8c7e7bfac312863d3b83cd3cdaec2a25d4f72
Ashutosh kumar <kumar.ashutosh920@gmail.com>
2020-04-30 19:09:09 +0530

    server.js updated

--- server.js
+++ server.js
@@ -82,6 +82,16 @@ io.on('connection', (socket) => {
   });
 });
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
